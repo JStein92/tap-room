af0656054e980bafeb5453a23b456080a2b0a59c
Guest User <Guest@epicodus-4.local>
2017-09-21 14:43:41 -0700

    More styling

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -25,48 +25,59 @@ import { DatePipe } from '@angular/common';
     <div *ngFor="let keg of childKegList | pintsRemaining:filterByPintsRemaining | kegStyle:filterByStyle" [class] = "priceColor(keg)">
       <h2>{{keg.brand}} - {{keg.name}} <button class = 'editBtn' (click)="editButtonHasBeenClicked(keg)">Edit</button> <button class = "btn btn-danger deleteBtn" (click)="deleteButtonClicked(keg)">X</button> </h2>
 
+      <h4>{{keg.style}} - {{keg.pintsRemaining}}/124</h4>
 
-
-
-      <h4>{{keg.style}}</h4>
-
-
-
-
-
-      <div class = "kegIcon">
-        <div class="progress vertical">
-            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
+    <div class = "kegBars">
+      <div class = 'row'>
+        <div class ='col-md-1 kegContainer'>
+            <div class = "kegIcon">
+              <div class = "vertical-container">
+                <div class="progress vertical">
+                    <div [class]="progressBarColor(keg)" role="progressbar"  aria-valuemin="0" aria-valuemax="100" [style.width]="keg.pintsRemaining/1.24">
+                    </div>
+                </div>
+              </div>
             </div>
+          </div>
+
+
+
+          <div class ='col-md-8'>
+          <p class ="ABV">ABV <span class="abvbolded">{{keg.alcoholContent.toFixed(2)}}%</span></p>
+            <div class="progress progress-abv">
+              <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuemin="0" aria-valuemax="20" [style.width]="keg.alcoholContent*5+'%'">
+              </div>
+            </div>
+            <div class="row pourButtons">
+            <h3 *ngIf="keg.price">
+              <div *ngIf="isHappyHour">
+
+              <button (click)="pour(keg)">Pour \${{(keg.price -1).toFixed(2)}}</button>
+              <button (click)="growler(keg)">Growler \${{((keg.price-1 )*growlerDiscount ).toFixed(2)}}</button>
+              <button (click)="largeGrowler(keg)">Lg. Growler \${{((keg.price -1)*lgGrowlerDiscount).toFixed(2)}}</button>
+
+              </div>
+              <div *ngIf="!isHappyHour">
+              <button (click)="pour(keg)">Pour \${{(keg.price).toFixed(2)}}</button>
+              <button (click)="growler(keg)">Growler \${{(keg.price*growlerDiscount).toFixed(2)}}</button>
+              <button (click)="largeGrowler(keg)">Lg. Growler \${{(keg.price*lgGrowlerDiscount).toFixed(2)}}</button>
+              </div>
+            </h3>
+            </div>
+
+          </div>
         </div>
+
       </div>
 
       <h3 *ngIf="!keg.price" class = 'bg-danger'>Not a valid price!</h3>
-        <span>ABV</span>
-      <div class="progress">
 
-        <div class="progress-bar progress-bar-striped active" role="progressbar"
-        aria-valuemin="0" aria-valuemax="20" [style.width]="keg.alcoholContent*5+'%'">
-          <p>{{keg.alcoholContent.toFixed(2)}}%</p>
-        </div>
-      </div>
 
-      <h4>{{keg.pintsRemaining}}</h4>
 
-      <h3 *ngIf="keg.price">
-        <div *ngIf="isHappyHour">
 
-        <button (click)="pour(keg)">Pour \${{(keg.price -1).toFixed(2)}}</button>
-        <button (click)="growler(keg)">Growler \${{((keg.price-1 )*growlerDiscount ).toFixed(2)}}</button>
-        <button (click)="largeGrowler(keg)">Lg. Growler \${{((keg.price -1)*lgGrowlerDiscount).toFixed(2)}}</button>
 
-        </div>
-        <div *ngIf="!isHappyHour">
-        <button (click)="pour(keg)">Pour \${{(keg.price).toFixed(2)}}</button>
-        <button (click)="growler(keg)">Growler \${{(keg.price*growlerDiscount).toFixed(2)}}</button>
-        <button (click)="largeGrowler(keg)">Lg. Growler \${{(keg.price*lgGrowlerDiscount).toFixed(2)}}</button>
-        </div>
-      </h3>
+
 
     </div>
   `
@@ -122,6 +133,18 @@ export class KegListComponent {
   }
   onStyleChange(optionFromMenu) {
     this.filterByStyle = optionFromMenu;
+  }
+
+  progressBarColor(keg){
+    if(keg.pintsRemaining < 40)
+    {
+        return "progress-bar progress-bar-danger";
+    }else if (keg.pintsRemaining >=40 && keg.pintsRemaining <=80){
+        return "progress-bar progress-bar-warning";
+    } else {
+        return "progress-bar progress-bar-success";
+    }
+
   }
 
   priceColor(keg){
